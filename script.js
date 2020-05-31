456bd6a08449c4811a8fbdd4af0a0c937a40b92c
Andre <andreoliveira.adv@hotmail.com>
2020-05-31 08:29:23 -0400

    Filling the modal with items and information

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ const ca = e => document.querySelectorAll(e);
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     
+    // Pizza elements
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     
+    // Pizza modal
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        
         
         c('.pizzaWindowArea').style.opacity = 0;        
         c('.pizzaWindowArea').style.display = 'flex';
